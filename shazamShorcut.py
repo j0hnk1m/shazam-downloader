4a0b4616a16c8704737761901a7202cf1a0ff5d0
teddyliang <teddytduoliang@gmail.com>
2019-03-09 13:17:52 -0800

    made file

---
+++ shazamShorcut.py
@@ -0,0 +1,13 @@
+import json
+import urllib.request as urllib2
+from bs4 import BeautifulSoup
+from google import google
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import os
+import pyscreenshot as Imagegrab
+import sys
+import wx
+from halo import Halo
