6a459583e7c2b0d30908f97eadab4389cf713a0b
John <kim.john228@gmail.com>
2019-03-09 22:39:53 -0800

    added thumbnail support

--- music.py
+++ music.py
@@ -1,11 +1,13 @@
 from __future__ import unicode_literals
 import os
-import sys
 import urllib
 import requests
 import re
 import youtube_dl
 import getpass
+from google_images_download import google_images_download
+from PIL import Image
+import eyed3
 
 
 def get_url(song, artist, lyrics):
@@ -21,9 +23,39 @@ def get_url(song, artist, lyrics):
 
 
 def download(url):
-	user = getpass.getuser()
-	os.chdir('/Users/' + user + '/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized')
+	os.chdir('/Users/' + getpass.getuser() + '/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized')
 	ydl_opts = {'format': 'bestaudio/best', 'postprocessors': [{'key': 'FFmpegExtractAudio',
 															'preferredcodec': 'mp3', 'preferredquality': '192'}]}
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		ydl.download([url])
+		info = ydl.extract_info(url, download=True)
+		filename = ydl.prepare_filename(info)[:-5] + '.mp3'
+	return os.getcwd() + '/' + filename
+
+
+def get_thumbnail(song, artist):
+	query = song + " " + artist
+	response = google_images_download.googleimagesdownload()
+	args = {'keywords': query, 'suffix_keywords': "album cover art", 'limit': 2, 'print_urls': True}
+	os.chdir('/Users/' + getpass.getuser() + '/Downloads')
+	img_paths = response.download(args)
+	
+	for k, v in img_paths.items():
+		v1 = Image.open(v[0])
+		v2 = Image.open(v[1])
+		
+		if v1.size[0] > v2.size[0]:
+			thumbnail = v[0]
+		else:
+			thumbnail = v[1]
+		
+		return thumbnail
+
+
+def set_thumbnail(mp3, thumbnail):
+	f = eyed3.load(mp3)
+	if f.tag is None:
+		f.initTag()
+	
+	f.tag.images.set(3, open(thumbnail, 'rb').read(), 'image/jpeg')
+	f.tag.save()
+
