413fc328ec364ead40feaac979596685fe403739
teddyliang <teddytduoliang@gmail.com>
2019-03-09 22:41:30 -0800

    edits

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import ocr
 import webbrowser
+from selenium import webdriver
+import pyscreenshot as ImageGrab
 
 
 # YOU NEED TO BE LOGGED IN BFEOREHAND BOTH ON MAC AND IPHONE
 webbrowser.open('https://www.shazam.com/myshazam')
-
+browser = webdriver.Chrome()
+browser.get("https://www.shazam.com/myshazam")
 
 def screenshot():
-	asdf
+
+	image = ImageGrab.grab(bbox=(10, 10, 510, 510))  # X1,Y1,X2,Y2
+
+
 
